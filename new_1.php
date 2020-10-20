dec9ae589296c1f108749e7fa0764ceec1f6da88
Egor914 <73187327+Egor914@users.noreply.github.com>
2020-10-20 19:04:11 +0300

    Add files via upload

---
+++ new_1.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+	<title>Home TECHNO | 914 Группа</title>
+	<meta http-equiv="Content-Type" content"text/html; charset=utf-8">
+ <style>
+   table {
+    width: 900px; /* Ширина таблицы */
+    border: 5px solid Purple; /* Рамка вокруг таблицы */
+    margin: auto; /* Выравниваем таблицу по центру окна  */
+   }
+   td {
+    text-align: center; /* Выравниваем текст по центру ячейки */
+   }
+  </style>
+ </head>
+ 
+ <body bgcolor="#0a0a0a" text="#fff" link="#FFFF0F">
+ <a href="/"><p align="center"><font size="25" color="#fff" face="sans-serif">Группа 914</font></p></a>
+ <figure class="passe-partout">
+ <table bgcolor="#1a162a">
+  <tr>
+   <th>
+    <img src="1.png" width="10%">
+	<hr>
+	 <p><font size="10">Иваночкин</font></p>
+	  <a href="https://vk.com/egorivanochkin"><br><font color="#ffffff" size="25">Купить билет</font></a>
+	<img src="2.png" width="70%">
+   </th>
+  </tr>
+ </table>
+ </figure>
+
+<audio id="audio" preload="" autoplay="autoplay" loop="3.mp3">
+    <source type="audio/mp3" src="3.mp3">
+  </audio>
+</body>
